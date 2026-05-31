bec00fc0d635c441d840dd81c605e6180bdfe0e0
zoya-debug <zoya31chaudhary@gmail.com>
2026-06-01 00:29:10 +0530

    final refined project

--- EDA.py
+++ EDA.py
@@ -3,21 +3,36 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("netflix_titles.csv")
-print(df.describe(include = "all"))
+print("Dataset Shape: ", df.shape)
+print(df.describe())
 
+#=================
+#Missing Values
+#=================
+print("Missing Values: \n")
+print(df.isnull().sum().sort_values(ascending = False))
+
+#==========================
 #Handling Missing Values
+#==========================
 df["director"] = df["director"].fillna("Unknown")
 df["country"] = df["country"].fillna("Not Available")
 df["cast"] = df["cast"].fillna("No Cast")
 
+#=======================================
 #Movies vs TV Show Distribution Plot
+#=======================================
 sns.countplot(x = df["type"])
 plt.title("Movies vs TV Shows Distribution")
 plt.xlabel("Type")
 plt.ylabel("Count")
+plt.tight_layout()
+plt.savefig("movies_vs_tvshows.png")
 plt.show()
 
+#============================
 #Rating Distribution Plot
+#============================
 plt.figure(figsize = (10,5))
 plt.xticks(rotation = 45)
 sns.countplot(data = df, x = "rating")
@@ -25,21 +40,30 @@ plt.title("Rating Distribution")
 plt.tight_layout()
 plt.xlabel("Rating")
 plt.ylabel("Count")
+plt.savefig("rating_distribution.png")
 plt.show()
 
+#==================================
 #Release Year Distribution Plot
+#==================================
 plt.figure(figsize=(12,5))
 sns.histplot(df["release_year"], bins=20)
 plt.title("Netflix Content Release Distribution")
 plt.xlabel("Release Year")
 plt.ylabel("Count")
+plt.tight_layout()
+plt.savefig("release_year_distribution.png")
 plt.show()
 
+#======================
 #Top 10 Genres Plot
+#======================
 genre_df = df.assign(Genre=df["listed_in"].str.split(", ")).explode("Genre")
 top_genre = genre_df["Genre"].value_counts().head(10)
 sns.barplot(x = top_genre.values, y = top_genre.index)
 plt.title("Top 10 Netflix Genres")
 plt.xlabel("No. of Titles")
 plt.ylabel("Genre")
+plt.tight_layout()
+plt.savefig("genre_plot.png")
 plt.show()
--- train_model.py
+++ train_model.py
@@ -6,10 +6,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-le_type = LabelEncoder()
-le_rating = LabelEncoder()
-le_director = LabelEncoder()
-
 df = pd.read_csv("netflix_titles.csv")
 
 df["director"] = df["director"].fillna("Unknown")
@@ -18,9 +14,20 @@ df["cast"] = df["cast"].fillna("No Cast")
 
 #print(df[["type", "rating", "release_year"]].isnull().sum())
 
+# ==========================
+# Data Loading
+# ==========================
 df_ml = df[["type", "rating", "release_year", "director"]].dropna()
+print("ML Dataset Shape: ", df_ml.shape)
+print("\nFeatures Used: ")
+print(["rating", "release_year", "director"])
 
-#Encoding
+# ==========================
+# Data Preprocessing
+# ==========================
+le_type = LabelEncoder()
+le_rating = LabelEncoder()
+le_director = LabelEncoder()
 df_ml["type"] = le_type.fit_transform(df_ml["type"])
 df_ml["rating"] = le_rating.fit_transform(df_ml["rating"])
 df_ml["director"] = le_director.fit_transform(df_ml["director"])
@@ -29,36 +36,58 @@ df_ml["director"] = le_director.fit_transform(df_ml["director"])
 X = df_ml[["rating", "release_year", "director"]]
 
 #Target
-Y = df_ml["type"]
+y = df_ml["type"]
 
 #Train Test Split
-X_train, X_test, Y_train, Y_test = train_test_split(
+X_train, X_test, y_train, y_test = train_test_split(
     X,
-    Y,
+    y,
     test_size = 0.2,
     random_state = 42
 )
 
+# ==========================
+# Model Training & Evaluation
+# ==========================
 #Decision Tree Model
 dt = DecisionTreeClassifier(random_state = 42)
-dt.fit(X_train, Y_train)
+dt.fit(X_train, y_train)
 dt_pred = dt.predict(X_test)
-print(dt_pred[:10])
-print("DT Accuracy = ", accuracy_score(Y_test, dt_pred))
-print("DT Confusion Matrix = \n", confusion_matrix(Y_test, dt_pred))
+print("\nDecision Tree Results: ")
+print("Accuracy = ", accuracy_score(y_test, dt_pred))
+print("Confusion Matrix = \n", confusion_matrix(y_test, dt_pred))
 
 #Random Forest Model
 rf = RandomForestClassifier(random_state = 42)
-rf.fit(X_train, Y_train)
+rf.fit(X_train, y_train)
 rf_pred = rf.predict(X_test)
-print(rf_pred[:10])
-print("RF Accuracy = ", accuracy_score(Y_test, rf_pred))
-print("RF Confusion Matrix = \n", confusion_matrix(Y_test, rf_pred))
+print("\nRandom Forest Results: ")
+print("Accuracy = ", accuracy_score(y_test, rf_pred))
+print("Confusion Matrix = \n", confusion_matrix(y_test, rf_pred))
 
 #Logistic regression Model
 lr = LogisticRegression(max_iter = 1000)
-lr.fit(X_train, Y_train)
+lr.fit(X_train, y_train)
 lr_pred = lr.predict(X_test)
-print(lr_pred[:10])
-print("LR Accuracy = ", accuracy_score(Y_test, lr_pred))
-print("LR Confusion Matrix = \n", confusion_matrix(Y_test, lr_pred))
+print("\nLogistic Regression Results: ")
+print("Accuracy = ", accuracy_score(y_test, lr_pred))
+print("Confusion Matrix = \n", confusion_matrix(y_test, lr_pred))
+
+# ==========================
+# Comparision Table
+# ==========================
+results = pd.DataFrame({
+    "Model": [
+        "Decision Tree",
+        "Random Forest",
+        "Logistic Regression"
+    ],
+    "Accuracy": [
+        accuracy_score(y_test, dt_pred),
+        accuracy_score(y_test, rf_pred),
+        accuracy_score(y_test, lr_pred)
+    ]
+})
+
+print("\nModel Comparison")
+print(results.sort_values("Accuracy", ascending = False))
